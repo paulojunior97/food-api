58cd30b888be4c9bc4e10ed9bf5363116a60e0bf
paulo junior <paulo_jr97@hotmail.com>
2022-03-18 21:32:10 -0300

    Tratando outras exceções não capturadas

--- src/main/java/com/paulo/dev/foodapi/api/controller/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/paulo/dev/foodapi/api/controller/exceptionhandler/ApiExceptionHandler.java
@@ -76,6 +76,19 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleUncaught(Exception ex, WebRequest request) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        ProblemTypeEnum problemType = ProblemTypeEnum.ERRO_DE_SISTEMA;
+        String detail = "Ocorreu um erro interno inesperado no sistema. Tente novamente e se o problema persistir, entre em contato com o administrador do sistema.";
+
+        ex.printStackTrace();
+
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+    }
+
     @Override
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
         if (body == null) {
